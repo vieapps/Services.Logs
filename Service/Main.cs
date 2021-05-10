f058407bf7168c23627965f4e2dc930e751e9252
Quynh Nguyen <mr.quynh@gmail.com>
2021-05-10 23:01:52 +0700

    (improv): alternative ways for writting logs

--- Service/Main.cs
+++ Service/Main.cs
@@ -2,6 +2,8 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Dynamic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -20,9 +22,7 @@ namespace net.vieapps.Services.Logs
 
 		string LogsPath { get; } = UtilityService.GetAppSetting("Path:Logs", "logs");
 
-		bool IsServiceLogsWriterDisabled { get; set; } = false;
-
-		bool IsServiceLogsFlusherDisabled { get; set; } = false;
+		bool IsServiceLogsDisabled { get; set; } = false;
 
 		bool CleaningServiceLogs { get; set; } = false;
 
@@ -30,51 +30,92 @@ namespace net.vieapps.Services.Logs
 
 		public override string ServiceName => "Logs";
 
-		public override Task RegisterServiceAsync(IEnumerable<string> args = null, Action<IService> onSuccess = null, Action<Exception> onError = null)
-			=> base.RegisterServiceAsync(args, async service =>
+		public override Task StartAsync(string[] args = null, bool initializeRepository = true, Action<IService> next = null)
+		{
+			if (args?.FirstOrDefault(arg => arg.IsStartsWith("/disable")) != null)
+				this.IsServiceLogsDisabled = true;
+			return base.StartAsync(args, initializeRepository, next);
+		}
+
+		public override async Task RegisterServiceAsync(IEnumerable<string> args = null, Action<IService> onSuccess = null, Action<Exception> onError = null)
+		{
+			if (!this.IsServiceLogsDisabled)
 			{
 				this.Logger.LogWarning("Initializing the instance of the logging service");
-
-				if (args?.FirstOrDefault(arg => arg.IsStartsWith("/disable-write")) != null)
-					this.IsServiceLogsWriterDisabled = true;
-				if (args?.FirstOrDefault(arg => arg.IsStartsWith("/disable-flush")) != null)
-					this.IsServiceLogsFlusherDisabled = true;
-
 				if (this.ServiceLogsInstance != null)
 					try
 					{
 						await this.ServiceLogsInstance.DisposeAsync().ConfigureAwait(false);
 					}
 					catch { }
-
 				this.ServiceLogsInstance = await Router.IncomingChannel.RealmProxy.Services.RegisterCallee<ILoggingService>(() => this, RegistrationInterceptor.Create()).ConfigureAwait(false);
-				this.Logger.LogWarning($"The instance of the logging service was successfully registered - [{this.LogsPath} - {this.IsServiceLogsWriterDisabled} - {this.IsServiceLogsFlusherDisabled}]");
+				this.Logger.LogWarning($"The instance of the logging service was successfully registered");
+			}
+			await base.RegisterServiceAsync(args, onSuccess, onError).ConfigureAwait(false);
+		}
 
-				onSuccess?.Invoke(service);
-			}, onError);
+		public override async Task UnregisterServiceAsync(IEnumerable<string> args, bool available = true, Action<IService> onSuccess = null, Action<Exception> onError = null)
+		{
+			if (this.ServiceLogsInstance != null)
+				try
+				{
+					await this.ServiceLogsInstance.DisposeAsync().ConfigureAwait(false);
+					this.Logger.LogWarning("The instance of the logging service was successfully unregistered");
+				}
+				catch (Exception ex)
+				{
+					this.Logger.LogError($"Error occurred while unregistering the instance of the logging service => {ex.Message}", ex);
+				}
+				finally
+				{
+					this.ServiceLogsInstance = null;
+				}
+			await base.UnregisterServiceAsync(args, available, onSuccess, onError).ConfigureAwait(false);
+		}
 
-		public override Task UnregisterServiceAsync(IEnumerable<string> args, bool available = true, Action<IService> onSuccess = null, Action<Exception> onError = null)
-			=> base.UnregisterServiceAsync(args, available, async service =>
-			{
-				if (this.ServiceLogsInstance != null)
-					try
+		public override async Task<JToken> ProcessRequestAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default)
+		{
+			using (var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, this.CancellationToken))
+				try
+				{
+					switch (requestInfo.ObjectName.ToLower())
 					{
-						await this.ServiceLogsInstance.DisposeAsync().ConfigureAwait(false);
-						this.Logger.LogWarning("The instance of the logging service was successfully unregistered");
-					}
-					catch (Exception ex)
-					{
-						this.Logger.LogError($"Error occurred while unregistering the instance of the logging service => {ex.Message}", ex);
-					}
-					finally
-					{
-						this.ServiceLogsInstance = null;
-					}
-				onSuccess?.Invoke(service);
-			}, onError);
+						case "service":
+						case "servicelog":
+						case "servicelogs":
+						case "service.log":
+						case "service.logs":
+							if (requestInfo.Verb.IsEquals("GET"))
+							{
+								var request = requestInfo.GetRequestExpando();
+								var pagination = request.Get<ExpandoObject>("Pagination");
+								var pageNumber = pagination.Get("PageNumber", 1);
+								var pageSize = pagination.Get("PageSize", 100);
+								var filterBy = request.Get<ExpandoObject>("FilterBy");
+								return await this.FetchLogsAsync(pageNumber > 0 ? pageNumber : 1, pageSize > 0 ? pageSize : 100, filterBy.Get<string>("CorrelationID"), filterBy.Get<string>("DeveloperID"), filterBy.Get<string>("AppID"), filterBy.Get<string>("ServiceName"), filterBy.Get<string>("ObjectName"), cts.Token).ConfigureAwait(false);
+							}
+							else if (requestInfo.Verb.IsEquals("POST"))
+							{
+								await this.WriteLogAsync(requestInfo.Body.FromJson<ServiceLog>(false, (log, _) =>
+								{
+									log.ID = string.IsNullOrWhiteSpace(log.ID) ? UtilityService.NewUUID : log.ID;
+									log.ServiceName = log.ServiceName?.ToLower();
+									log.ObjectName = log.ObjectName?.ToLower();
+								}), cts.Token).ConfigureAwait(false);
+								return new JObject();
+							}
+							else
+								throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.GetURI()}]");
 
-		public override Task<JToken> ProcessRequestAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default)
-			=> Task.FromResult<JToken>(null);
+						default:
+							throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.GetURI()}]");
+					}
+				}
+				catch (Exception ex)
+				{
+					throw this.GetRuntimeException(requestInfo, ex);
+				}
+		}
 
 		protected override async Task ProcessInterCommunicateMessageAsync(CommunicateMessage message, CancellationToken cancellationToken = default)
 		{
@@ -112,88 +153,64 @@ namespace net.vieapps.Services.Logs
 			}
 		}
 
+		Task WriteLogAsync(ServiceLog log, CancellationToken cancellationToken)
+			=> this.WriteLogsAsync(new[] { log }, cancellationToken);
+
+		Task WriteLogsAsync(IEnumerable<ServiceLog> logs, CancellationToken cancellationToken)
+			=> logs.ForEachAsync(async log =>
+			{
+				var filePath = Path.Combine(this.LogsPath, $"logs.services.{DateTime.Now:yyyyMMddHHmmss}.{UtilityService.NewUUID}.json");
+				await UtilityService.WriteTextFileAsync(filePath, log.ToString(Formatting.Indented), false, null, cancellationToken).ConfigureAwait(false);
+			}, true, false);
+
 		public Task WriteLogAsync(string correlationID, string developerID, string appID, string serviceName, string objectName, string log, string stack = null, CancellationToken cancellationToken = default)
 			=> this.WriteLogsAsync(correlationID, developerID, appID, serviceName, objectName, string.IsNullOrWhiteSpace(log) ? null : new List<string> { log }, stack, cancellationToken);
 
-		public async Task WriteLogsAsync(string correlationID, string developerID, string appID, string serviceName, string objectName, List<string> logs, string stack = null, CancellationToken cancellationToken = default)
-		{
-			if (this.IsServiceLogsWriterDisabled)
-				return;
-
-			var loggingItem = new ServiceLog
-			{
-				CorrelationID = correlationID,
-				DeveloperID = string.IsNullOrWhiteSpace(developerID) ? null : developerID,
-				AppID = string.IsNullOrWhiteSpace(appID) ? null : appID,
-				ServiceName = (string.IsNullOrWhiteSpace(serviceName) ? "APIGateway" : serviceName).ToLower(),
-				ObjectName = (string.IsNullOrWhiteSpace(objectName) || objectName.IsEquals(serviceName) ? "" : objectName).ToLower(),
-				Logs = "" + logs?.Where(log => !string.IsNullOrWhiteSpace(log)).Join("\r\n"),
-				Stack = string.IsNullOrWhiteSpace(stack) ? null : stack
-			};
-
-			using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3)))
-			{
-				using (var ctsrc = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, this.CancellationToken))
+		public Task WriteLogsAsync(string correlationID, string developerID, string appID, string serviceName, string objectName, List<string> logs, string stack = null, CancellationToken cancellationToken = default)
+				=> this.WriteLogAsync(new ServiceLog
 				{
-					var filePath = Path.Combine(this.LogsPath, $"logs.services.{DateTime.Now:yyyyMMddHHmmss}.{UtilityService.NewUUID}.json");
-					await UtilityService.WriteTextFileAsync(filePath, loggingItem.ToString(Formatting.Indented), false, null, ctsrc.Token).ConfigureAwait(false);
-				}
-			}
-		}
+					CorrelationID = correlationID,
+					DeveloperID = string.IsNullOrWhiteSpace(developerID) ? null : developerID,
+					AppID = string.IsNullOrWhiteSpace(appID) ? null : appID,
+					ServiceName = (string.IsNullOrWhiteSpace(serviceName) ? "APIGateway" : serviceName).ToLower(),
+					ObjectName = (string.IsNullOrWhiteSpace(objectName) || objectName.IsEquals(serviceName) ? "" : objectName).ToLower(),
+					Logs = "" + logs?.Where(log => !string.IsNullOrWhiteSpace(log)).Join("\r\n"),
+					Stack = string.IsNullOrWhiteSpace(stack) ? null : stack
+				}, cancellationToken);
 
 		async Task FlushLogsAsync()
 		{
-			if (this.IsServiceLogsFlusherDisabled)
-				return;
-
 			var filePaths = Directory.EnumerateFiles(this.LogsPath, "logs.services.*.json").ToList();
 			if (filePaths.Count > 0)
 			{
 				if (this.IsDebugLogEnabled)
 					this.Logger.LogDebug($"Flush service logs from {filePaths.Count:###,###,##0} files");
 
-				using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(90)))
+				var logs = new List<ServiceLog>();
+				await filePaths.ForEachAsync(async filePath =>
 				{
-					using (var ctsrc = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, this.CancellationToken))
+					try
 					{
-						var logs = new List<ServiceLog>();
-						await filePaths.ForEachAsync(async filePath =>
+						using (var reader = new StreamReader(filePath))
 						{
-							try
-							{
-								using (var reader = new StreamReader(filePath))
-								{
-									var json = await reader.ReadToEndAsync(ctsrc.Token).ConfigureAwait(false);
-									logs.Add(json.FromJson<ServiceLog>());
-								}
-							}
-							catch (Exception ex)
-							{
-								this.Logger.LogError($"Error occurred while reading JSON file => {ex.Message}", ex);
-							}
-							File.Delete(filePath);
-						}).ConfigureAwait(false);
-						await logs.OrderBy(log => log.Time).ForEachAsync(async log => await ServiceLog.CreateAsync(log, ctsrc.Token).ConfigureAwait(false), true, false).ConfigureAwait(false);
+							var json = await reader.ReadToEndAsync(this.CancellationToken).ConfigureAwait(false);
+							logs.Add(json.FromJson<ServiceLog>());
+						}
 					}
-				}
+					catch (Exception ex)
+					{
+						this.Logger.LogError($"Error occurred while reading JSON file => {ex.Message}", ex);
+					}
+					File.Delete(filePath);
+				}).ConfigureAwait(false);
+				await this.FlushLogsAsync(logs, this.CancellationToken).ConfigureAwait(false);
 			}
 		}
 
-		/*
-		public Task WriteLogsAsync(string correlationID, string developerID, string appID, string serviceName, string objectName, List<string> logs, string stack = null, CancellationToken cancellationToken = default)
-		{
-			this.Logger.LogDebug($"Write log => {logs.Join("\r\n")}");
-			return Task.CompletedTask;
-		}
+		Task FlushLogsAsync(IEnumerable<ServiceLog> logs, CancellationToken cancellationToken)
+			=> logs.Where(log => log != null).OrderBy(log => log.Time).ForEachAsync(async log => await ServiceLog.CreateAsync(log, cancellationToken).ConfigureAwait(false), true, false);
 
-		Task FlushLogsAsync()
-		{
-			this.Logger.LogDebug($"Flush logs");
-			return Task.CompletedTask;
-		}
-		*/
-
-		public async Task<JToken> FetchLogsAsync(int pageNumber = 1, int pageSize = 100, string correlationID = null, string developerID = null, string appID = null, string serviceName = null, string objectName = null, CancellationToken cancellationToken = default)
+		async Task<JToken> FetchLogsAsync(int pageNumber, int pageSize, string correlationID, string developerID, string appID, string serviceName, string objectName, CancellationToken cancellationToken)
 		{
 			var filter = Filters<ServiceLog>.And();
 			if (!string.IsNullOrWhiteSpace(correlationID))
